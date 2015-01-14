6d0522caa9359ea1451c7cc1eb0d498d3c2655b1
Jason Robert Fincham <JRFincham@yahoo.com>
2015-01-13 19:31:46 -0500

    Initial Commit

---
+++ Readme.md
@@ -0,0 +1,1 @@
+# Answering some questions for a homework assignment
---
+++ main.js
@@ -0,0 +1,42 @@
+var name;
+// Q: What does name equal right now?
+
+
+name = "Tim";
+// Q: What is `name` set to right now?
+// A: Tim
+
+name = "Bill";
+// Q: What is `name` set to right now?
+// A: Bill
+
+var greeting = "Hello, how are you?";
+// Q: What is `greeting` set to?
+// A: "Hello, how are you?"
+
+var high = 50;
+var low  = 10;
+
+
+var math = high - low;
+// Q: What is `math` set to?
+// A: 40
+
+math = high - "5";
+// Q: What is `math` set to?
+// A: 45
+
+// Create a variable to calculate my age
+// The answer should read "Tim is 31 years old"
+var born = 1983;
+var today = 2015;
+// A: var age = today - born - 1
+    A: "Tim is " + age + " years old"
+
+
+
+// Store some information following in variables.
+var yourname = "Jason";
+var myname = "Tim";
+
+var statement = yourname + " is taking a class at The Iron Yard, and my instructor is " + myname;
